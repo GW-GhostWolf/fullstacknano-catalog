164765da1c8c1b5c52383c8c2e2918557b27a44e
GW-GhostWolf <michael.poc@att.net>
2018-04-06 13:55:12 -0500

    add item view page

--- flaskserver.py
+++ flaskserver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Web server for categories application using flask
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import json
 
 from sqlalchemy import create_engine
@@ -17,7 +17,7 @@ session = dbSession()
 
 
 @app.route("/")
-@app.route("/categories")
+@app.route("/categories", methods=["GET"])
 def getCategories():
     categories = session.query(Category)\
             .join("items")\
@@ -25,24 +25,51 @@ def getCategories():
     return render_template("categories.html", categories=categories)
 
 
-@app.route("/category/<int:catId>/item/<int:itemId>")
-def editItem(catId, itemId):
-    category = session.query(Category).filter(Category.id==catId).one()
-    if(itemId == 0):
+@app.route("/category/<int:catId>/newItem", methods=["GET", "POST"])
+def newItem(catId):
+    if request.method == 'POST':
+        # TODO - save the new item
+        ""
+    else:
+        category = session.query(Category).filter(Category.id==catId).one()
         item = Item(id=0, name="", description="", cat_id=category.id)
         return render_template("editItem.html", category=category, item=item)
-    item = session.query(Item).filter(Item.id==itemId).one()
-    return render_template("editItem.html", category=category, item=item)
 
 
-@app.route("/categories.json")
+@app.route("/item/<int:itemId>", methods=["GET"])
+def viewItem(itemId):
+    item = session.query(Item).join("category").filter(Item.id==itemId).one()
+    return render_template("item.html", item=item)
+
+
+@app.route("/item/<int:itemId>/edit", methods=["GET", "POST"])
+def editItem(itemId):
+    if request.method == 'POST':
+        # TODO - save the updates to the existing item
+        ""
+    else:
+        item = (session.query(Item).join("category")
+            .filter(Item.id==itemId).one())
+        return render_template("editItem.html",
+            category=item.category, item=item)
+
+
+@app.route("/item/<int:itemId>/delete", methods=["GET", "POST"])
+def deleteItem(itemId):
+    if request.method == 'POST':
+        # TODO - delete the item
+        ""
+    else:
+        item = (session.query(Item).join("category")
+            .filter(Item.id==itemId).one())
+        return render_template("deleteItem.html", item=item)
+
+
+@app.route("/categories.json", methods=["GET"])
 def CategoryJson():
     categories = session.query(Category).join("items").all()
     return jsonify(Categories=[c.serializable for c in categories])
 
-@app.route("/catalog/<string:category_name>/")
-def CategoryName(category_name):
-    return "Hello World!"
 
 if(__name__ == "__main__"):
     app.debug = True
