7fa68271361263804e16c126ace94dd04ef552be
GW-GhostWolf <michael.poc@att.net>
2018-04-08 17:40:37 -0500

    add user table

--- db_configuration.py
+++ db_configuration.py
@@ -8,12 +8,21 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 
+class User(Base):
+    """Create user table for storing user information"""
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(256), nullable=False)
+    name = Column(String(256), nullable=False)
+
+
 class Category(Base):
     """Create the table that will store the category information"""
     __tablename__ = "categories"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     items = relationship("Item")
+    user_id = Column(Integer, ForeignKey("users.id"))
 
     @property
     def serializable(self):
@@ -21,7 +30,8 @@ class Category(Base):
         return {
             "id": self.id,
             "name": self.name,
-            "items": [i.serializable for i in self.items]
+            "items": [i.serializable for i in self.items],
+            "userid": self.user_id
         }
 
 
@@ -33,6 +43,7 @@ class Item(Base):
     description = Column(String(512))
     cat_id = Column(Integer, ForeignKey("categories.id"))
     category = relationship(Category, back_populates="items")
+    user_id = Column(Integer, ForeignKey("users.id"))
 
     @property
     def serializable(self):
@@ -40,9 +51,10 @@ class Item(Base):
         return {
             "id": self.id,
             "name": self.name,
-            "description": self.description
+            "description": self.description,
+            "userid": self.user_id
         }
 
 
-engine = create_engine("sqlite:///categlog.db")
+engine = create_engine("sqlite:///catelog.db")
 Base.metadata.create_all(engine)
